5cacd0bfa828ebedd4d67a553e103abb752942db
coderek <zen1986@gmail.com>
2017-04-20 12:29:51 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { ReaderComponent } from './components/reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ReaderComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ReaderComponent]
 })
 export class AppModule { }
---
+++ src/app/components/reader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  
+  
+  `,
+  styleUrls: ['./reader.component.css']
+})
+export class ReaderComponent {
+}
