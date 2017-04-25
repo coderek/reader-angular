1b783f8e26a32f74f8006b292faea021857075b3
coderek <zen1986@gmail.com>
2017-04-25 10:53:30 -0400

    Added reader service

---
+++ reader_service/reader_service.py
@@ -0,0 +1,34 @@
+# py3
+import json
+import sys
+from flask import Flask, request, make_response
+
+sys.path.append('/home/coderek/Code/reader/reader_service')
+
+from feed_service import fetch_feed
+
+from flask_cors import CORS
+
+
+counter = 1
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=['POST'])
+def feed_fetcher():
+    url = request.json['url']
+    global counter
+    feed = fetch_feed(url, counter)
+    if url == "test_feed":
+        counter+=1
+    
+    serialized = json.dumps(feed, ensure_ascii=False)
+    resp = make_response(serialized)
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+
+
+if __name__ == "__main__":
+    app.run()
